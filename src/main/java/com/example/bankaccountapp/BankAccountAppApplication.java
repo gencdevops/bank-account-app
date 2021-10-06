34b9a94269f4751976cff6720c90cad77995bcac
gencdevops <gencdevops@gmail.com>
2021-10-06 22:53:52 +0300

    Initial commit

---
+++ src/main/java/com/example/bankaccountapp/BankAccountAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankaccountapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankaccountapp/BankAccountAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankaccountapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
